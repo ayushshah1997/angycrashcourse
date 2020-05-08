9a3f331a30609008269694399ae19f7d8de6f26d
ayushshah1997 <ayushshah1997@gmail.com>
2020-05-08 16:17:26 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from "@angular/core";
-
+import { todo} from "./todo";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
@@ -8,5 +8,25 @@ import { Component, VERSION } from "@angular/core";
 export class AppComponent {
   name = "Angular " + VERSION.major;
   me: string = "Ayush Shah";
-  
+  todoVal : string;
+  list: todo[];
+ ngOnInit(){
+   this.list =[]
+   this.todoVal = null;
+ }
+
+ addItem(){
+   if(this.todoVal!==null){
+     const item : todo ={
+      id:this.list.length+1,
+      value: this.todoVal,
+      isDone : false
+     }
+     this.list.push(item);
+   }
+ }
+
+ deleteItem(id : number){
+   
+ }
 }
